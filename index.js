1404b701f8d0c269c076a2a1e4d06f8bac57545a
Frank Tianyu Zeng <tianyuzeng1997@gmail.com>
2020-10-18 03:58:12 -0700

    finish auth

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
+const { request } = require('express')
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/User')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express()
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, ( accessToken, refreshToken, profile, done ) => {
-        console.log('access token', accessToken)
-        console.log('refresh token', refreshToken)
-        console.log('profile', profile)
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 )
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
+require('./routes/authRoutes')(app)
 
-app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
